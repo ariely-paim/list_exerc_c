6e4fb91bfd895e65059b759115e47dd7ad291261
Arielly Paim <112345620+ariely-paim@users.noreply.github.com>
2024-10-05 11:18:34 -0300

    Update questao4.c

--- questao4.c
+++ questao4.c
@@ -11,15 +11,15 @@ Crie uma estrutura representando os alunos de um determinado curso. A estrutura 
 #include <string.h>
 
 typedef struct {
-int matricula;
-char nome[100];
-float nota1;
-float nota2;
-float nota3;
+  int matricula;
+  char nome[100];
+  float nota1;
+  float nota2;
+  float nota3;
 } Aluno;
 
 float calcular_media(float nota1, float nota2, float nota3) {
-return (nota1 + nota2 + nota3) / 3;
+  return (nota1 + nota2 + nota3) / 3;
 }
 
 int main() {
@@ -31,83 +31,83 @@ int Maior_media = 0;
 int Menor_media = 0;
 
 for (i = 0; i < max_alunos; i++) {
-printf("Informe o número de matrícula do aluno %d: ", i + 1);
-scanf("%d", &alunos[i].matricula);
+  printf("Informe o número de matrícula do aluno %d: ", i + 1);
+  scanf("%d", &alunos[i].matricula);
 
-printf("Informe o nome do aluno %d: ", i + 1);
-getchar();
-fgets(alunos[i].nome, sizeof(alunos[i].nome), stdin);
-alunos[i].nome[strlen(alunos[i].nome)-1 ] = '\0';
-printf("Digite a nota da primeira prova do aluno %d: ", i + 1);
-scanf("%f", &alunos[i].nota1);
+  printf("Informe o nome do aluno %d: ", i + 1);
+  getchar();
+  fgets(alunos[i].nome, sizeof(alunos[i].nome), stdin);
+  alunos[i].nome[strlen(alunos[i].nome)-1 ] = '\0';
+  printf("Digite a nota da primeira prova do aluno %d: ", i + 1);
+  scanf("%f", &alunos[i].nota1);
 
-printf("Digite a nota da segunda prova do aluno %d: ", i + 1);
-scanf("%f", &alunos[i].nota2);
+  printf("Digite a nota da segunda prova do aluno %d: ", i + 1);
+  scanf("%f", &alunos[i].nota2);
 
-printf("Digite a nota da terceira prova do aluno %d: ", i + 1);
-scanf("%f", &alunos[i].nota3);
+  printf("Digite a nota da terceira prova do aluno %d: ", i + 1);
+  scanf("%f", &alunos[i].nota3);
 
-printf("\n");
+  printf("\n");
 }
 
 for (int i = 1; i < max_alunos; i++) {
-if (alunos[i].nota1 > alunos[Maior_nota1].nota1) {
-Maior_nota1 = i;
-}
-}
-
-float maior_media = calcular_media(alunos[0].nota1, alunos[0].nota2, alunos[0].nota3);
-float menor_media = maior_media;
-
-for (i = 1; i < max_alunos; i++) {
-float media = calcular_media(alunos[i].nota1, alunos[i].nota2, alunos[i].nota3);
-
-if (media > maior_media) {
-maior_media = media;
-Maior_media = i;
+  if (alunos[i].nota1 > alunos[Maior_nota1].nota1) {
+    Maior_nota1 = i;
+  }
 }
 
-if (media < menor_media) {
-menor_media = media;
-Menor_media = i;
-}
+  float maior_media = calcular_media(alunos[0].nota1, alunos[0].nota2, alunos[0].nota3);
+  float menor_media = maior_media;
 
-}
+  for (i = 1; i < max_alunos; i++) {
+    float media = calcular_media(alunos[i].nota1, alunos[i].nota2, alunos[i].nota3);
+
+    if (media > maior_media) {
+      maior_media = media;
+      Maior_media = i;
+    }
+
+    if (media < menor_media) {
+      menor_media = media;
+      Menor_media = i;
+    }
+
+  }
 
 
-printf("---Dados dos alunos---\n");
-for (i = 0; i < max_alunos; i++) {
-float media = calcular_media(alunos[i].nota1, alunos[i].nota2, alunos[i].nota3);
-char result;
-printf("Aluno %d:\n", i + 1);
-printf("Sua matrícula: %d\n", alunos[i].matricula);
-printf("Seu nome: %s\n", alunos[i].nome);
-printf("Sua primeira nota: %.2f\n", alunos[i].nota1);
-printf("Sua segunda nota: %.2f\n", alunos[i].nota2);
-printf("sua terceira nota: %.2f\n", alunos[i].nota3);
-printf("Sua média: %.2f\n", media);
-if (media >= 6) {
-result = "Aprovado.";
-printf(result);
-} else {
-result = "Reprovado.";
-printf(result);
-}
-printf("Seu status: %s\n", result);
-printf("\n");
-}
+  printf("---Dados dos alunos---\n");
+  for (i = 0; i < max_alunos; i++) {
+    float media = calcular_media(alunos[i].nota1, alunos[i].nota2, alunos[i].nota3);
+    char result;
+    printf("Aluno %d:\n", i + 1);
+    printf("Sua matrícula: %d\n", alunos[i].matricula);
+    printf("Seu nome: %s\n", alunos[i].nome);
+    printf("Sua primeira nota: %.2f\n", alunos[i].nota1);
+    printf("Sua segunda nota: %.2f\n", alunos[i].nota2);
+    printf("sua terceira nota: %.2f\n", alunos[i].nota3);
+    printf("Sua média: %.2f\n", media);
+    if (media >= 6) {
+      result = "Aprovado.";
+      printf(result);
+    } else {
+      result = "Reprovado.";
+      printf(result);
+    }
+    printf("Seu status: %s\n", result);
+    printf("\n");
+  }
 
-printf("---Aluno com maior nota na primeira prova---\n");
-printf("Seu nome: %s\n", alunos[Maior_nota1].nome);
-printf("Sua primeira nota: %.2f\n\n", alunos[Maior_nota1].nota1);
+  printf("---Aluno com maior nota na primeira prova---\n");
+  printf("Seu nome: %s\n", alunos[Maior_nota1].nome);
+  printf("Sua primeira nota: %.2f\n\n", alunos[Maior_nota1].nota1);
 
-printf("--Aluno com maior média geral---\n");
-printf("Seu nome: %s\n", alunos[Maior_media].nome);
-printf("Sua média: %.2f\n\n", maior_media);
+  printf("--Aluno com maior média geral---\n");
+  printf("Seu nome: %s\n", alunos[Maior_media].nome);
+  printf("Sua média: %.2f\n\n", maior_media);
 
-printf("---Aluno com menor média geral---\n");
-printf("Seu nome: %s\n", alunos[Menor_media].nome);
-printf("Sua média: %.2f\n", menor_media);
+  printf("---Aluno com menor média geral---\n");
+  printf("Seu nome: %s\n", alunos[Menor_media].nome);
+  printf("Sua média: %.2f\n", menor_media);
 
-return 0;
+  return 0;
 }
