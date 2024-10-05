b1bb3d38735643a2b3f82064d4a4143a21d0c8d9
Arielly Paim <112345620+ariely-paim@users.noreply.github.com>
2024-10-05 11:19:42 -0300

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -3,26 +3,26 @@
 #include <string.h>
 
 typedef struct {
-char nome[100];
-int idade;
-char endereco[100];
+  char nome[100];
+  int idade;
+  char endereco[100];
 } Pessoa;
 
 int main (){
-Pessoa pessoa;
+  Pessoa pessoa;
 
-printf("Informe o seu nome: ");
-fgets(pessoa.nome, 100, stdin);
-pessoa.nome[strlen(pessoa.nome)-1 ]= '\0';
+  printf("Informe o seu nome: ");
+  fgets(pessoa.nome, 100, stdin);
+  pessoa.nome[strlen(pessoa.nome)-1 ]= '\0';
 
-printf("Informe o seu endereço: ");
-fgets(pessoa.endereco, 100, stdin);
-pessoa.endereco[strlen(pessoa.endereco)-1 ] = '\0';
+  printf("Informe o seu endereço: ");
+  fgets(pessoa.endereco, 100, stdin);
+  pessoa.endereco[strlen(pessoa.endereco)-1 ] = '\0';
 
-printf("Informe a sua idade: ");
-scanf("%d", &pessoa.idade);
+  printf("Informe a sua idade: ");
+  scanf("%d", &pessoa.idade);
 
-printf("\nO seu nome é: %s\n", pessoa.nome);
-printf("A sua idade é: %d\n", pessoa.idade);
-printf("O seu endereço é: %s\n", pessoa.endereco);
+  printf("\nO seu nome é: %s\n", pessoa.nome);
+  printf("A sua idade é: %d\n", pessoa.idade);
+  printf("O seu endereço é: %s\n", pessoa.endereco);
 }
